f16f457617e3f39b2f8ae355f47cc7ee43962f21
David <shogunuz@gmail.com>
2020-06-01 17:24:30 +0700

    v4.4

--- src/app/worker-details/worker-holiday/worker-holiday.component.ts
+++ src/app/worker-details/worker-holiday/worker-holiday.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {ToastrService} from "ngx-toastr";
 import {WorkerHolidayService} from "../../shared/worker-holiday.service";
@@ -14,18 +14,21 @@ export class WorkerHolidayComponent implements OnInit {
    minDateEnd: Date;
    maxDateEnd: Date;
    Datess: Date;
-
+  keynumber: number=0;
   constructor(public serviceHoliday:WorkerHolidayService,
               private toaster: ToastrService)
     {
-      this.minDateStart = new Date();
-      this.minDateEnd = new Date();
-      this.maxDateStart = new Date();
-      this.maxDateEnd = new Date();
-      this.minDateStart.setDate(this.minDateStart.getDate() + 1);
-      this.maxDateStart.setDate(this.maxDateStart.getDate() + 300);
-      this.minDateEnd.setDate(this.minDateEnd.getDate() + 2);
-      this.maxDateEnd.setDate(this.maxDateEnd.getDate() + 365);
+      if(this.keynumber==0){
+        this.minDateStart = new Date();
+        this.minDateEnd = new Date();
+        this.maxDateStart = new Date();
+        this.maxDateEnd = new Date();
+        this.minDateStart.setDate(this.minDateStart.getDate() + 1);
+        this.maxDateStart.setDate(this.maxDateStart.getDate() + 300);
+        this.minDateEnd.setDate(this.minDateEnd.getDate() + 3);
+        this.maxDateEnd.setDate(this.maxDateEnd.getDate() + 365);
+        this.keynumber = 1;
+      }
     }
 
   ngOnInit(): void {
@@ -59,12 +62,25 @@ export class WorkerHolidayComponent implements OnInit {
     this.serviceHoliday.postWorkerHoliday().subscribe(
       res => {
         this.resetForm(formHoliday);
-        this.toaster.success('Submitted successfully', 'Holiday has been registered');
+        this.toaster.success('Успешно!', 'Работник отправится на отпуск!');
         this.serviceHoliday.refreshListOfHolidays();
       },
       err =>{
-        console.log(err)
+        this.toaster.error('Возможно, Вы пытаетесь отправить в отпуск уже отправленного сотрудника, либо выше определенного лимита! ', 'Произошла ошибка!');
+        this.serviceHoliday.refreshListOfHolidays();
       }
     )
+  }
+  gettingMinDate(){
+    //if(this.serviceHoliday.formDataForHoliday.DateStart != null)
+      this.minDateEnd = new Date(this.serviceHoliday.formDataForHoliday.DateStart);
+      this.minDateEnd.setDate(this.minDateEnd.getDate() + 1);
+  }
+  //@ViewChild('DateEnd', {})
+
+  cleaningEndDate(DateEnd){
+    this.serviceHoliday.formDataForHoliday.DateEnd = null;
+    //this.serviceHoliday.formDataForHoliday
+    DateEnd.value = null;
   }
 }
